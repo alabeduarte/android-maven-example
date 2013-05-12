72a24b552ab2f8bab8d00e345ad01856a5026a31
Adriano Bonat <adrianob@gmail.com>
2013-05-12 20:53:14 -0300

    initial commit.
    command used to create project:
    mvn archetype:generate -DarchetypeArtifactId=android-release \
                       -DarchetypeGroupId=de.akquinet.android.archetypes \
                       -DarchetypeVersion=1.0.9 \
                       -DgroupId=com.example \
                       -DartifactId=android-maven-release-archetype

---
+++ android-maven-release-archetype-it/src/main/java/com/example/test/HelloAndroidActivityTest.java
@@ -0,0 +1,17 @@
+package com.example.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import com.example.*;
+
+public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
+
+    public HelloAndroidActivityTest() {
+        super(HelloAndroidActivity.class); 
+    }
+
+    public void testActivity() {
+        HelloAndroidActivity activity = getActivity();
+        assertNotNull(activity);
+    }
+}
+
